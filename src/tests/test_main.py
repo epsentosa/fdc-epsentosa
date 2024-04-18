d0ab499f361b0f9069ed3ac154051414365bd788
Eko Putra Sentosa <ekoputra@bangunindo.com>
2024-04-18 15:58:49 +0700

    feat: test clickhouse connection

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,6 +1,11 @@
-from fig_data_challenge.main import return_42
+# from fig_data_challenge.main import return_42
+from connections.clickhouse import client
+
+# def test_main():
+#     value = return_42()
+#     assert value == 42
 
 
-def test_main():
-    value = return_42()
-    assert value == 42
+def test_clickhouse_connection():
+    value = client.command("SELECT 1")
+    assert value == 1
