9a1139d019eff5e611f791181914bde5b9199d65
Eko Putra Sentosa <ekoputra@bangunindo.com>
2024-04-18 16:00:09 +0700

    feat: main entrypoint process etl and migration

--- src/fig_data_challenge/main.py
+++ src/fig_data_challenge/main.py
@@ -1,8 +1,30 @@
-# Write a function that prints "Hello, World!" to the console.
-def return_42() -> int:
-    return 42
+import argparse
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="subparser_name")
+    parser_run = subparsers.add_parser("run")
+    parser_run.add_argument(
+        "mode",
+        choices=[
+            "db-migration",
+            "start-etl",
+        ],
+    )
+
+    args, unknownargs = parser.parse_known_args()
+
+    if args.subparser_name == "run" and args.mode == "db-migration":
+        from db_migration.clickhouse import start_db_migration
+
+        start_db_migration()
+
+    elif args.subparser_name == "run" and args.mode == "start-etl":
+        from fig_data_challenge.etl import master_etl
+
+        master_etl()
 
 
 if __name__ == "__main__":
-    print("Hello World")
-    return_42()
+    main()
